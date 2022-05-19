7e04461a40ceadfabb06cfa913f1fcf8c3c5c556
AnthonyR <arankin789@gmail.com>
2022-05-19 11:13:11 -0400

    setup outline for app

--- Develop/server.js
+++ Develop/server.js
@@ -8,9 +8,42 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, "public", "index.html" ))
+    res.sendFile(path.join(__dirname, './public/index.html' ))
 })
 
+app.get("/notes", (req, res) =>{
+    res.sendFile(path.join(__dirname, "public", "notes.html"))
+});
+
+// Function to create a new note
+function newNote (){
+
+}
+
+// post request for new note
+app.post("/notes", (req, res) =>{
+
+})
+
+// Function to update a note
+function updateNote (){
+
+}
+
+// PUT request to update note
+app.put("/notes/:id", (req, res) =>{
+
+})
+
+// Function to delete a note
+function deleteNote () {
+
+}
+
+// Delete request to delete note
+app.delete("/notes/:id", (req, res) =>{
+    
+})
 
 
 app.listen(PORT, () =>{
